076f0a151ef28dc8e2248b81c9bca53527daec02
Martin Davis <martin_davis3@yahoo.ca>
2020-04-11 13:30:39 -0600

    add App constructor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import '../styles/App.css';
-import {Fragment} from 'react'
+import { Fragment } from 'react'
+import { IUserLogin } from '../models/userLogins'
 
-function App() {
+export interface IAppProps {
+}
+
+export interface IState {
+  userLogins: IUserLogin[];
+  loggedIn: boolean;
+  currUser: string;
+}
+
+export default class App extends React.Component<IAppProps, IState> {
+  constructor(props: IAppProps) {
+    super(props);
+    
+    this.state = {
+      userLogins: [
+        {
+          username: 'user1',
+          password: 'pass1'
+        }
+      ],
+      loggedIn: false,
+      currUser: 'default'
+    }
+
+  }
+
+
+
+public render() {
   return (
     <Fragment>
       <h1>Hello, world!</h1>
@@ -10,4 +39,6 @@ function App() {
   );
 }
 
-export default App;
+}
+
+// export default App;
