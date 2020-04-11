7f338b89c068af247a3f9cb4355283e6784257c5
Martin Davis <martin_davis3@yahoo.ca>
2020-04-11 12:32:15 -0600

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/App.css';
+import {Fragment} from 'react'
+
+function App() {
+  return (
+    <Fragment>
+      <h1>Hello, world!</h1>
+    </Fragment>
+  );
+}
+
+export default App;
