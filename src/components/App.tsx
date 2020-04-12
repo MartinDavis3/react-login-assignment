9685bde6215369240f7655dbab2fe63d685b49db
Martin Davis <martin_davis3@yahoo.ca>
2020-04-11 23:07:40 -0600

    add login checking

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -35,23 +35,38 @@ export default class App extends React.Component<IAppProps, IState> {
   }
 
   public onClickLogInButton = () => {
-    this.setState( {loginFormIsOpen: true})
+    this.setState( { loginFormIsOpen: true } );
     // this.setState( { loggedIn: true } )
   }
   
   public onClickLogOutButton = () => {
-    this.setState( { loggedIn: false} )
+    this.setState( { loggedIn: false} );
   }
 
   public onClickSubmitButton = (currUserInp: string, currPassInp: string) => {
-    this.setState( {
-      currUser: currUserInp,
-      currPass: currPassInp,
-      loginFormIsOpen: false} );
+    this.setState( { loginFormIsOpen: false } );
+    if (this.isValidUser( currUserInp, currPassInp ) ) {
+      this.setState( {
+        loggedIn: true,
+        currUser: currUserInp,
+        currPass: currPassInp
+      } );
+    }
+  }
+
+  private isValidUser = (currUserInp:string, currPassInp: string): boolean => {
+    let {userLogins} = this.state
+    var matchFound = false
+    for ( let i = 0; i < userLogins.length; i++ ) {
+      if ( currUserInp === userLogins[i].username && currPassInp === userLogins[i].password ) {
+        matchFound = true;
+      }
+    }
+    return matchFound;
   }
 
 public render() {
-  let { userLogins, loginFormIsOpen, loggedIn, currUser, currPass } = this.state;
+  let { loginFormIsOpen, loggedIn } = this.state;
   return (
     <Fragment>
       <h1>Hello, world!</h1>
